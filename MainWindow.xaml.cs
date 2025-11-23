bca81e1bfa5c4e6cbd91d27db34d415bee7cfddd
MrRoblick Robloxer <robloxerplayer1999@gmail.com>
2025-11-23 19:02:49 +0400

    Updated

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -13,14 +17,9 @@ namespace Gluon;
 /// </summary>
 public partial class MainWindow : Window
 {
-    public enum AttachStatus
-    {
-        None,
-        Attached,
-        Failed
-    }
-    private static AttachStatus _currentStatus = AttachStatus.None;
-    private static AttachStatus _oldStatus = AttachStatus.None;
+    private const string ProcessName = "gmod";
+    private static HttpClient _httpClient = new();
+    private static ProcessStartInfo _injectorStartInfo = new("bin/injector.exe", $"{ProcessName}.exe bin/gluonapi.dll");
     public MainWindow()
     {
         InitializeComponent();
@@ -28,6 +27,7 @@ public partial class MainWindow : Window
         using var reader = new XmlTextReader(s);
         CodeEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
     }
+    
 
     private void TopPanel_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
@@ -48,15 +48,69 @@ public partial class MainWindow : Window
         WindowState = WindowState.Minimized;
     }
 
-    private void AttachButton_OnClick(object sender, RoutedEventArgs e)
+    private async void AttachButton_OnClick(object sender, RoutedEventArgs e)
     {
-        _currentStatus = AttachStatus.Attached;
-        MessageBox.Show(_currentStatus == _oldStatus ? "Already attached" : _currentStatus == AttachStatus.Attached ? "Attached" : "Failed");
-        _oldStatus = _currentStatus;
+        try
+        {
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+            try
+            {
+                await _httpClient.GetStringAsync("http://127.0.0.1:8887", cts.Token);
+                MessageBox.Show("Already attached");
+                return;
+            } catch (OperationCanceledException) { }
+            
+            var processes = Process.GetProcessesByName(ProcessName);
+            if (processes.Length == 0)
+            {
+                MessageBox.Show($"Failed to find process {ProcessName}.exe");
+                return;
+            }
+            using var process = Process.Start(_injectorStartInfo);
+            if(process == null) return;
+            await process.WaitForExitAsync();
+            using var cts2 = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+            try
+            {
+                await _httpClient.GetStringAsync("http://127.0.0.1:8887", cts2.Token);
+                MessageBox.Show("Attached");
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("Failed to attach");
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
     }
 
-    private void ExecuteButton_OnClick(object sender, RoutedEventArgs e)
+    private async void ExecuteButton_OnClick(object sender, RoutedEventArgs e)
     {
-        
+        try
+        {
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+            var processes = Process.GetProcessesByName(ProcessName);
+            if (processes.Length == 0)
+            {
+                MessageBox.Show($"Open the {ProcessName}.exe");
+                return;
+            }
+
+            try
+            {
+                await _httpClient.PostAsync("http://127.0.0.1:8887/execute",
+                    new StringContent(CodeEditor.Text, Encoding.ASCII, "text/plain"), cts.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("Not attached");
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
     }
 }
